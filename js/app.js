bf95e80bd5cefaa782027b89f1314bdddcf5b19a
eboye <eboyee@gmail.com>
2017-02-16 00:02:28 +0100

    add responsive menu

--- js/app.js
+++ js/app.js
@@ -338,7 +338,12 @@
     /* OTA page only */
     $(function () {
         if ($('body').hasClass('ota')) {
-
+            $('.button-collapse').sideNav({
+                menuWidth: 300, // Default is 300
+                edge: 'right', // Choose the horizontal origin
+                closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                draggable: true // Choose whether you can drag to open on touch screens
+            });
             $('.modal').modal();
             $.getJSON(domain + config, function (data) {
                 homePageRender(data);
